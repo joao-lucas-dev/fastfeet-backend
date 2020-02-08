7b1c0adea2fe2470fe96a3aa99b4ec99955f61f0
Joao Lucas <jlmelosouza18@gmail.com>
2020-02-08 20:05:33 -0300

    feat: update recipients

--- src/app/controllers/RecipientController.js
+++ src/app/controllers/RecipientController.js
@@ -21,10 +21,10 @@ class RecipientController {
 
     const { cpf } = req.body;
 
-    const user = await Recipient.findOne({ where: { cpf } });
+    const recipient = await Recipient.findOne({ where: { cpf } });
 
-    if (user) {
-      return res.status(400).json({ error: 'User already exits!' });
+    if (recipient) {
+      return res.status(400).json({ error: 'Recipient already exits!' });
     }
 
     const {
@@ -37,6 +37,58 @@ class RecipientController {
       city,
       cep,
     } = await Recipient.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      cpf,
+      street,
+      number,
+      complement,
+      city,
+      state,
+      cep,
+    });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      cpf: Yup.string(),
+      street: Yup.string(),
+      number: Yup.string(),
+      complement: Yup.string(),
+      state: Yup.string(),
+      city: Yup.string(),
+      cep: Yup.string(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails!' });
+    }
+
+    const { id } = req.params;
+    const { cpf } = req.body;
+
+    const recipient = await Recipient.findByPk(id);
+
+    if (recipient.cpf !== cpf) {
+      const recipientExists = await Recipient.findOne({ where: { cpf } });
+
+      if (recipientExists) {
+        return res.status(400).json({ error: 'Recipient already exits!' });
+      }
+    }
+
+    const {
+      name,
+      street,
+      number,
+      complement,
+      state,
+      city,
+      cep,
+    } = await recipient.update(req.body);
 
     return res.json({
       id,
